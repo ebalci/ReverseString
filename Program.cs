b7d3170f7cad08704181eb8e45fb0cc1b75aa623
ebalci <erol.balci@gmail.com>
2012-09-16 20:05:42 +1000

    v1 Reversing a String

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReverseString
+{
+  class Program
+  {
+    static void Main( string[] args )
+    {
+      Console.Write( "Please Enter the string to reverse: " );
+      string line = Console.ReadLine();
+      Reverse reverse = new Reverse();
+
+      do
+      {
+        Console.Write( "Reversed using a simple loop: " );
+        Console.WriteLine( reverse.Rev(line) );
+        Console.Write( "Reversed using recursion:  " );
+        Console.WriteLine( reverse.RecursiveRev(line) );
+
+        line = Console.ReadLine();
+      }
+      while ( line != String.Empty );
+      
+    }
+
+  }
+}
---
+++ Reverse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReverseString
+{
+  class Reverse
+  {
+    /// <summary>
+    /// Reverses a string using a simple for loop
+    /// </summary>
+    /// <param name="text">The string to be reversed</param>
+    /// <returns>The string with characters in reverse order</returns>
+    public string Rev( string text )
+    {     
+      char[] cArray = text.ToCharArray();
+      string reverse = String.Empty;
+      for ( int i = 1; i <= cArray.Length; i++ )
+      {
+        reverse += cArray[cArray.Length - i];
+      }
+
+      return reverse;
+    }
+
+    /// <summary>
+    /// Reverses a string using recursion
+    /// </summary>
+    /// <param name="text">The string to be reversed</param>
+    /// <returns>The string with characters in reverse order</returns>
+    public string RecursiveRev( string s )
+    {
+      // Return when the end of the string is reached
+      if ( (s == null) || (s.Length <= 1) )
+      {
+        return s;
+      }
+      
+      // Substring starting at index 1 + first character on the end
+      return RecursiveRev( s.Substring(1) ) + s[0];
+    }
+  }
+}
