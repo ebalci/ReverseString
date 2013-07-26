18d187957f61d0b1927b6ec234948eda73130a09
Alethor <oscarbralo@outlook.com>
2013-07-26 10:15:05 +0200

    Update Reverse.cs
    Hi!!
    
    I did a few changes, using StringBuilder better than string +=, because string in C# are inmutable, and when you do:
    string a = "a";
    a += "b"; 
    You are creating a new string with "ab", and so on, but iwth StringBuilder it doesn´t happend, using Append() method.
    
    I have removed RecursionRev too, I think that for most part of cases with Rev will be enough, is O(n) and I think is enough unless you have to process a really huge text.
    
    I hope you like

--- Reverse.cs
+++ Reverse.cs
@@ -1,66 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
 using System.Text;
 
 namespace ReverseString
 {
-  class Reverse
-  {
-    /// <summary>
-    /// Reverses a string using a simple for loop
-    /// </summary>
-    /// <param name="text">The string to be reversed</param>
-    /// <returns>The string with characters in reverse order</returns>
-    public string Rev( string text )
-    {     
-      char[] cArray = text.ToCharArray();
-      string reverse = String.Empty;
-      for ( int i = 1; i <= cArray.Length; i++ )
-      {
-        reverse += cArray[cArray.Length - i];
-      }
-
-      return reverse;
-    }
-
-    /// <summary>
-    /// Reverses a string using recursion
-    /// </summary>
-    /// <param name="text">The string to be reversed</param>
-    /// <returns>The string with characters in reverse order</returns>
-    public string RecursiveRev( string str )
+    class Reverse
     {
-      // Return when the end of the string is reached
-      if ( (str == null) || (str.Length <= 1) )
-      {
-        return str;
-      }
-      
-      // Substring starting at index 1 + first character on the end
-      return RecursiveRev( str.Substring(1) ) + str[0];
+        /// <summary>
+        /// Reverses a string using a simple for loop
+        /// </summary>
+        /// <param name="text">The string to be reversed</param>
+        /// <returns>The string with characters in reverse order</returns>
+        public string Rev(string text)
+        {
+            StringBuilder reverse = new StringBuilder("");
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reverse.Append(text[i]);
+            }
+
+            return reverse.ToString();
+        }
+
+        /// <summary>
+        /// Reverses the order of the words in a sentence
+        /// </summary>
+        /// <param name="str">The input string</param>
+        /// <returns>Returns the sentence with the words in reverse order</returns>
+        public string ReverseSentence(string str)
+        {
+            str = Rev(str);
+            // Reverse the entire line of text
+            // Split the reversed sentence and store each word
+            string[] words = str.Split(' ');
+
+            StringBuilder sr = new StringBuilder("");
+            // Now reverse every word back to there originals
+            for (int i = 0; i < words.Length; i++)
+            {
+                sr.Append(Rev(words[i]) + " ");
+            }
+
+            // Remove the last whithespace
+            sr.Remove(sr.Length - 1, 1); 
+            return sr.ToString();
+        }
     }
-
-    /// <summary>
-    /// Reverses the order of the words in a sentence
-    /// </summary>
-    /// <param name="str">The input string</param>
-    /// <returns>Returns the sentence with the words in reverse order</returns>
-    public string ReverseSentence( string str )
-    {
-      // Reverse the entire line of text
-      str = RecursiveRev(str);
-      // Split the reversed sentence and store each word
-      string[] words = str.Split(' ');
-
-      str = String.Empty;
-      // Now reverse every word back to there originals
-      for( int i=0; i< words.Length; i++ )
-      {
-        str += RecursiveRev(words[i]) + " ";
-      }
-
-      return str;
-    }
-  }
 }
